f6fa0210d2f02c23a21592508704974903933ed9
przemekpiotrowski007-pixel <przemek.piotrowski007@gmail.com>
2026-09-08 08:16:47 +0200

    Initialize Android project with Kotlin and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
 
+android {
+    namespace = "pl.nygus.live"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "pl.nygus.live"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
